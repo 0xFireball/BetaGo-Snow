3d0448e73f16f4cd3d33cd0466f068e6fa473d02
0xFireball <0xFireball@outlook.com>
2016-12-10 21:00:37 -0800

    fix layout issues

--- app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
+++ app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
@@ -3,9 +3,11 @@ package xyz.iridiumion.betagosnow.ui
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.res.TypedArray
+import android.graphics.Color
 import android.support.annotation.AttrRes
 import android.support.annotation.StyleRes
 import android.support.v7.view.ContextThemeWrapper
+import android.text.*
 import android.util.TypedValue
 import android.view.View
 import android.widget.Button
@@ -24,67 +26,33 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
         val SIGNUP_PERSPECTIVE_ID = View.generateViewId()
     }
 
-    val View.contextThemeWrapper: ContextThemeWrapper
-        get() = context.contextThemeWrapper
-
-    val Context.contextThemeWrapper: ContextThemeWrapper
-        get() = when (this) {
-            is ContextThemeWrapper -> this
-            is ContextWrapper -> baseContext.contextThemeWrapper
-            else -> throw IllegalStateException("Context is not an Activity, can't get theme: $this")
-        }
-
-    @StyleRes
-    fun View.attrStyle(@AttrRes attrColor: Int): Int = contextThemeWrapper.attrStyle(attrColor)
-
-    @StyleRes
-    private fun ContextThemeWrapper.attrStyle(@AttrRes attrRes: Int): Int =
-            attr(attrRes) {
-                it.getResourceId(0, 0)
-            }
-
-    private fun <R> ContextThemeWrapper.attr(@AttrRes attrRes: Int, block: (TypedArray) -> R): R {
-        val typedValue = TypedValue();
-        if (!theme.resolveAttribute(attrRes, typedValue, true)) throw IllegalArgumentException("$attrRes is not resolvable")
-        val a = obtainStyledAttributes(typedValue.data, intArrayOf(attrRes));
-        val result = block(a)
-        a.recycle()
-        return result
-    }
-
     override fun createView(ui: AnkoContext<AuthenticateActivity>) = with(ui) {
 
         verticalLayout {
             id = LAYOUT_ID
             padding = dip(30)
-            val loginPerspective = linearLayout {
+            val loginPerspective = verticalLayout {
                 id = LOGIN_PERSPECTIVE_ID
                 editText {
                     hint = "Username"
-                    textSize = 24f
                 }
                 editText {
                     hint = "Password"
-                    textSize = 24f
                 }
                 button("Connect") {
-                    textSize = 26f
-                    attrStyle(R.attr.borderlessButtonStyle)
                 }
             }
             loginPerspective.visibility = View.GONE
-            val signupPerspective = linearLayout {
+            val signupPerspective = verticalLayout {
                 id = SIGNUP_PERSPECTIVE_ID
                 editText {
                     hint = "Username"
-                    textSize = 24f
                 }
                 editText {
                     hint = "Password"
-                    textSize = 24f
+                    inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
                 }
                 button("Register") {
-                    textSize = 26f
                 }
             }
             signupPerspective.visibility = View.GONE
@@ -96,13 +64,16 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
 
             val authPickerPerspective = linearLayout {
                 val btnRegister = button("Register")
+                        .lparams(width = wrapContent)
                 val btnLogin = button("Log In")
+                        .lparams(width = wrapContent)
                 btnLogin.onClick {
                     loginPerspective.visibility = View.VISIBLE
                     signupPerspective.visibility = View.GONE
                     btnRegister.visibility = View.VISIBLE
                     btnLogin.visibility = View.GONE
                     btnRegister.text = "Register Instead"
+                    btnRegister.lparams(width = matchParent)
                 }
                 btnRegister.onClick {
                     loginPerspective.visibility = View.GONE
@@ -110,13 +81,13 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
                     btnRegister.visibility = View.GONE
                     btnLogin.visibility = View.VISIBLE
                     btnLogin.text = "Log In Instead"
+                    btnLogin.lparams(width = matchParent)
                 }
             }
         }.applyRecursively {
             view ->
             when (view) {
                 is Button -> {
-
                 }
             }
         }
