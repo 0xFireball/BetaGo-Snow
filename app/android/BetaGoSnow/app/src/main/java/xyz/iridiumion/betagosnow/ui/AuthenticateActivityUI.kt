61fe275bdc6c61c7ca9135246fe9779b560a7203
0xFireball <0xFireball@outlook.com>
2016-12-10 19:21:20 -0800

    resolve styles from anko

--- app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
+++ app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
@@ -1,8 +1,17 @@
 package xyz.iridiumion.betagosnow.ui
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.TypedArray
+import android.support.annotation.AttrRes
+import android.support.annotation.StyleRes
+import android.support.v7.view.ContextThemeWrapper
+import android.util.TypedValue
 import android.view.View
+import android.widget.Button
 import org.jetbrains.anko.*
 import xyz.iridiumion.betagosnow.AuthenticateActivity
+import xyz.iridiumion.betagosnow.R
 
 /**
  * Author: 0xFireball, IridiumIon Software
@@ -15,7 +24,36 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
         val SIGNUP_PERSPECTIVE_ID = View.generateViewId()
     }
 
+    val View.contextThemeWrapper: ContextThemeWrapper
+        get() = context.contextThemeWrapper
+
+    val Context.contextThemeWrapper: ContextThemeWrapper
+        get() = when (this) {
+            is ContextThemeWrapper -> this
+            is ContextWrapper -> baseContext.contextThemeWrapper
+            else -> throw IllegalStateException("Context is not an Activity, can't get theme: $this")
+        }
+
+    @StyleRes
+    fun View.attrStyle(@AttrRes attrColor: Int): Int = contextThemeWrapper.attrStyle(attrColor)
+
+    @StyleRes
+    private fun ContextThemeWrapper.attrStyle(@AttrRes attrRes: Int): Int =
+            attr(attrRes) {
+                it.getResourceId(0, 0)
+            }
+
+    private fun <R> ContextThemeWrapper.attr(@AttrRes attrRes: Int, block: (TypedArray) -> R): R {
+        val typedValue = TypedValue();
+        if (!theme.resolveAttribute(attrRes, typedValue, true)) throw IllegalArgumentException("$attrRes is not resolvable")
+        val a = obtainStyledAttributes(typedValue.data, intArrayOf(attrRes));
+        val result = block(a)
+        a.recycle()
+        return result
+    }
+
     override fun createView(ui: AnkoContext<AuthenticateActivity>) = with(ui) {
+
         verticalLayout {
             id = LAYOUT_ID
             padding = dip(30)
@@ -31,6 +69,7 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
                 }
                 button("Connect") {
                     textSize = 26f
+                    attrStyle(R.attr.borderlessButtonStyle)
                 }
             }
             loginPerspective.visibility = View.GONE
@@ -71,6 +110,13 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
                     btnRegister.visibility = View.GONE
                     btnLogin.visibility = View.VISIBLE
                     btnLogin.text = "Log In Instead"
+                }
+            }
+        }.applyRecursively {
+            view ->
+            when (view) {
+                is Button -> {
+
                 }
             }
         }
