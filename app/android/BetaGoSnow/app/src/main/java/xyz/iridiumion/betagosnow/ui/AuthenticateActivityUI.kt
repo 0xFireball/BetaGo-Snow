51047a56e955b3a03bd7bc2561054e41f6cf048b
0xFireball <0xFireball@outlook.com>
2016-12-10 21:26:02 -0800

    optimally space login layout

--- app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
+++ app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/AuthenticateActivityUI.kt
@@ -1,19 +1,12 @@
 package xyz.iridiumion.betagosnow.ui
 
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.res.TypedArray
-import android.graphics.Color
-import android.support.annotation.AttrRes
-import android.support.annotation.StyleRes
-import android.support.v7.view.ContextThemeWrapper
-import android.text.*
-import android.util.TypedValue
+import android.text.InputType
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import org.jetbrains.anko.*
 import xyz.iridiumion.betagosnow.AuthenticateActivity
-import xyz.iridiumion.betagosnow.R
 
 /**
  * Author: 0xFireball, IridiumIon Software
@@ -63,26 +56,31 @@ class AuthenticateActivityUI : AnkoComponent<AuthenticateActivity> {
                 ui.owner.finish()
             }
 
-            val authPickerPerspective = linearLayout {
-                val btnRegister = button("Register")
-                        .lparams(width = wrapContent)
-                val btnLogin = button("Log In")
-                        .lparams(width = wrapContent)
-                btnLogin.onClick {
-                    loginPerspective.visibility = View.VISIBLE
-                    signupPerspective.visibility = View.GONE
-                    btnRegister.visibility = View.VISIBLE
-                    btnLogin.visibility = View.GONE
-                    btnRegister.text = "Register Instead"
-                    btnRegister.lparams(width = matchParent)
-                }
-                btnRegister.onClick {
-                    loginPerspective.visibility = View.GONE
-                    signupPerspective.visibility = View.VISIBLE
-                    btnRegister.visibility = View.GONE
-                    btnLogin.visibility = View.VISIBLE
-                    btnLogin.text = "Log In Instead"
-                    btnLogin.lparams(width = matchParent)
+            frameLayout {
+                val authPickerPerspective = linearLayout {
+                    val btnRegister = button("Register")
+                    {
+                    }.lparams(weight = 1f) {
+                    }
+                    val btnLogin = button("Log In") {
+                    }.lparams(weight = 1f) {
+                    }
+                    btnLogin.onClick {
+                        loginPerspective.visibility = View.VISIBLE
+                        signupPerspective.visibility = View.GONE
+                        btnRegister.visibility = View.VISIBLE
+                        btnLogin.visibility = View.GONE
+                        btnRegister.text = "Register Instead"
+                        btnRegister.lparams(width = matchParent)
+                    }
+                    btnRegister.onClick {
+                        loginPerspective.visibility = View.GONE
+                        signupPerspective.visibility = View.VISIBLE
+                        btnRegister.visibility = View.GONE
+                        btnLogin.visibility = View.VISIBLE
+                        btnLogin.text = "Log In Instead"
+                        btnLogin.lparams(width = matchParent)
+                    }
                 }
             }
         }.applyRecursively {
