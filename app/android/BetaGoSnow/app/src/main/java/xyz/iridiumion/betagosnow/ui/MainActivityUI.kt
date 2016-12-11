31509c8ab15736bf81f1710961b53247f834ecc6
0xFireball <0xFireball@outlook.com>
2016-12-10 18:33:13 -0800

    create mainactivity anko ui

--- app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/MainActivityUI.kt
+++ app/android/BetaGoSnow/app/src/main/java/xyz/iridiumion/betagosnow/ui/MainActivityUI.kt
@@ -1,5 +1,6 @@
 package xyz.iridiumion.betagosnow.ui
 
+import android.view.View
 import org.jetbrains.anko.*
 import xyz.iridiumion.betagosnow.MainActivity
 
@@ -8,10 +9,17 @@ import xyz.iridiumion.betagosnow.MainActivity
  */
 
 class MainActivityUI : AnkoComponent<MainActivity> {
+    companion object {
+        val LAYOUT_ID = View.generateViewId()
+        val BTN_REGISTER_ID = View.generateViewId()
+    }
+
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         verticalLayout {
+            id = LAYOUT_ID
             val name = editText()
             button("Say Hello") {
+                id = BTN_REGISTER_ID
                 onClick { ctx.toast("Hello, ${name.text}!") }
             }
         }
